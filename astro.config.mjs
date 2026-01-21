16145407e5d0871891c1bf976a948d24d3c609c2
Tim Jensen <56789534+tjensen42@users.noreply.github.com>
2026-01-21 14:57:53 +0100

    feat: add github actions deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://timjensen.de",
   vite: {
     plugins: [tailwindcss()],
   },
