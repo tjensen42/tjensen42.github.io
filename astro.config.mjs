48e7e02fdc6243cf737f949a1d756fd36ecdfe08
Tim Jensen <56789534+tjensen42@users.noreply.github.com>
2026-01-21 17:19:13 +0100

    fix: add cname file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 // Get the repo name for GitHub Pages base path
 // Remove this base config once you have a custom domain set up
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    site: 'https://www.timjensen.de',
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
